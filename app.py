9ca39f21bb1027c9c4ca235b38cc4a4b29de2afb
Kenia0894 <120056540+Kenia0894@users.noreply.github.com>
2023-12-06 19:53:03 -0500

    Add files via upload
    actualizado

---
+++ app.py
@@ -0,0 +1,22 @@
+from flask import Flask, render_template
+import requests
+app = Flask(__name__)
+
+def get_pokemon_data(pokemon):
+    url = f'https://pokeapi.co/api/v2/pokemon/{pokemon}'
+    r = requests.get(url).json()
+    return r 
+
+
+@app.route("/home")
+def home():
+    return render_template('pokemon.html')
+
+@app.route("/detalle")
+def detalle():
+    return render_template('detalle.html')
+
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
